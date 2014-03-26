c3db45e4b127d961a51f6996a9232310c149d63e
Nicholas Angeli <nicangeli@gmail.com>
2014-03-26 13:11:58 +0000

    init commit

--- one_app.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'mechanize'
-require 'pp'
-
-agent = Mechanize.new
-page = agent.get('https://play.google.com/store/apps/details?id=uk.co.focusmm.dts_theory')
-
-email = page.link_with(:text => 'Email Developer')
-pp email.href
