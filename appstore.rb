f0e1575a85f8e549c7b7f814c70b3f3a4c6631eb
Nicholas Angeli <nicangeli@gmail.com>
2014-03-27 16:56:09 +0000

    app store

--- appstore.rb
+++ appstore.rb
@@ -6,31 +6,25 @@ require 'json'
 require 'public_suffix'
 require 'uri/http'
 
-agent = Mechanize.new { |agent|
+$agent = Mechanize.new { |agent|
   agent.user_agent_alias = 'Mac Safari'
 }
 
-page = agent.get('https://itunes.apple.com/gb/genre/ios-education/id6017?mt=8&letter=A')
-links = page.links.find_all do |l|
-   l.attributes.attributes["href"].value.start_with?("https://itunes.apple.com/gb/app")
-end
-
+page = $agent.get('https://itunes.apple.com/gb/genre/ios-education/id6017?mt=8&letter=B')
 apps = []
-links.each do |link|
+
+def get_app_details link
     app = Hash.new
-    app_page = link.click
-    begin
-        app[:title] = app_page.search('h1').text.strip
-    rescue
-    end
-    begin
-        app[:author] = app_page.search('#title h2').text.strip
-    rescue
-    end
+    agent = Mechanize.new { | agent | 
+        agent.user_agent_alias = 'Mac Safari'
+    }
+    app_page = agent.get(link.href)
+    app[:title] = app_page.search('h1').text.strip
+    app[:author] = app_page.search('#title h2').text.strip
     begin
         app[:website] = app_page.link_with(:dom_class => 'see-all').href.chomp.strip
     rescue
-        app[:web] = app_page.link_with(:text => /.*Support/).href.chomp.strip
+        app[:website] = app_page.link_with(:text => /.*Support/).href.chomp.strip
     end
     begin
         uri = URI.parse(app[:website])
@@ -39,16 +33,34 @@ links.each do |link|
         w = Whois.whois(domain)
         p = w.parser
         admin = p.admin_contacts
-        app[:admin_email] = admin[0].email
-    rescue => error
-    ensure
-        begin
-            app[:registrant_contacts] = p.registrant_contacts[0].email
-        rescue
-        end
+        registrant = p.registrant_contacts
+        app[:email] = admin[0].email || registrant[0].email
+    rescue
     end
-    pp app
-    apps << app
+
+    if app[:email]
+        return app
+    end
 end
 
-pp apps.to_json
+
+until $agent.page.link_with(:text => 'Next').nil? do 
+    links = page.links.find_all do |l|
+        l.attributes.attributes["href"].value.start_with?("https://itunes.apple.com/gb/app")
+    end
+    links.each do |l|
+        app_details =  get_app_details(l)
+        if app_details
+            apps << app_details
+            pp app_details
+        end
+    end
+
+    pp 'Moving on to next page'
+    # agent.page.links
+    page = $agent.page.link_with(:text => 'Next').click
+end
+
+File.open('appstore_b.json', 'w') do |f|
+    f.write(apps.to_json)
+end
